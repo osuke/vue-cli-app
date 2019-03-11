ee318b9eebabcd5075ad9b4b12fa5a363ea38a4e
OsukeUesugi <funxion.mail@gmail.com>
2019-03-12 00:34:09 +0900

    added GA

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import 'reset.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+const { VUE_APP_GA_ID } = process.env;
+
+if (VUE_APP_GA_ID) {
+  Vue.use(VueAnalytics, {
+    id: VUE_APP_GA_ID,
+    router,
+  });
+}
 
 Vue.config.productionTip = false;
 
